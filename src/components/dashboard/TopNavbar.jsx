6c636b6ec933a4b11250e5cbe8891c6ad49a2085
mihret-emeru <mercyemeru@gmail.com>
2026-07-22 20:21:20 +0300

    Add TopNavbar component for dashboard layout

--- src/components/dashboard/TopNavbar.jsx
+++ src/components/dashboard/TopNavbar.jsx
@@ -1,1 +1,14 @@
+"use client";
+
+export default function TopNavbar() {
+  return (
+    <header className="top-navbar">
+      <h2>Dashboard</h2>
+
+      <div className="top-navbar-right">
+        {/* User profile will go here later */}
+      </div>
+    </header>
+  );
+}
 
