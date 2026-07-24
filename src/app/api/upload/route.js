3d9db541cf6bd4661bd2a83bf80ba30eeb8a848a
mihret-emeru <mercyemeru@gmail.com>
2026-07-24 21:12:51 +0300

    Add POST route for image uploads
    Implement image upload functionality to Cloudinary.

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,1 +1,52 @@
+import { NextResponse } from "next/server";
+import cloudinary from "@/lib/cloudinary";
+
+export async function POST(request) {
+  try {
+    const formData = await request.formData();
+
+    const files = formData.getAll("images");
+
+    const uploadPromises = files.map(async (file) => {
+      const bytes = await file.arrayBuffer();
+
+      const buffer = Buffer.from(bytes);
+
+      return new Promise((resolve, reject) => {
+        cloudinary.uploader
+          .upload_stream(
+            {
+              folder: "real-estate-properties",
+            },
+
+            (error, result) => {
+              if (error) {
+                reject(error);
+              }
+
+              resolve(result.secure_url);
+            },
+          )
+          .end(buffer);
+      });
+    });
+
+    const imageUrls = await Promise.all(uploadPromises);
+
+    return NextResponse.json({
+      success: true,
+      images: imageUrls,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: error.message,
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+}
 
