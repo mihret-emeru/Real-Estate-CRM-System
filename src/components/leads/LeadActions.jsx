751e06ed1eb3784f14fafa0fc843f96c64d8d527
mihret-emeru <mercyemeru@gmail.com>
2026-07-28 20:29:11 +0300

    Add LeadActions component for lead management

--- src/components/leads/LeadActions.jsx
+++ src/components/leads/LeadActions.jsx
@@ -1,1 +1,23 @@
+"use client";
+
+import Link from "next/link";
+import { FaEye, FaEdit, FaTrash } from "react-icons/fa";
+
+export default function LeadActions({ leadId, onDelete }) {
+  return (
+    <div className="lead-actions">
+      <Link href={`/manager/leads/${leadId}`} className="lead-view-btn">
+        <FaEye />
+      </Link>
+
+      <Link href={`/manager/leads/${leadId}/edit`} className="lead-edit-btn">
+        <FaEdit />
+      </Link>
+
+      <button className="lead-delete-btn" onClick={() => onDelete(leadId)}>
+        <FaTrash />
+      </button>
+    </div>
+  );
+}
 
