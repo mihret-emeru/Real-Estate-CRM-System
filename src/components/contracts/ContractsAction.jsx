e48403a160ba62bf95d20f896c0d84acb38bd7d5
mihret-emeru <mercyemeru@gmail.com>
2026-07-30 19:38:39 +0300

    Add contract action buttons for view, edit, upload, and delete

--- src/components/contracts/ContractsAction.jsx
+++ src/components/contracts/ContractsAction.jsx
@@ -1,1 +1,43 @@
+"use client";
+
+import Link from "next/link";
+import { FaEye, FaEdit, FaTrash, FaUpload, FaDownload } from "react-icons/fa";
+
+export default function ContractActions({ contractId, onDelete }) {
+  return (
+    <div className="contract-actions">
+      <Link
+        href={`/manager/contracts/${contractId}`}
+        className="contract-view-btn"
+      >
+        <FaEye />
+      </Link>
+
+      <Link
+        href={`/manager/contracts/${contractId}/edit`}
+        className="contract-edit-btn"
+      >
+        <FaEdit />
+      </Link>
+
+      <Link
+        href={`/manager/contracts/${contractId}/upload`}
+        className="contract-upload-btn"
+      >
+        <FaUpload />
+      </Link>
+
+      <button className="contract-download-btn">
+        <FaDownload />
+      </button>
+
+      <button
+        className="contract-delete-btn"
+        onClick={() => onDelete(contractId)}
+      >
+        <FaTrash />
+      </button>
+    </div>
+  );
+}
 
