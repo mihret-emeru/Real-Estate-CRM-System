f71c3b70f127557396312a7277f3127886cfb940
mihret-emeru <mercyemeru@gmail.com>
2026-07-31 20:25:08 +0300

    Add function to generate contract number

--- src/utils/generateContractNumber.js
+++ src/utils/generateContractNumber.js
@@ -1,1 +1,12 @@
+import Contract from "@/models/Contract";
+
+export default async function generateContractNumber() {
+  const year = new Date().getFullYear();
+
+  const count = await Contract.countDocuments();
+
+  const sequence = String(count + 1).padStart(4, "0");
+
+  return `CTR-${year}-${sequence}`;
+}
 
