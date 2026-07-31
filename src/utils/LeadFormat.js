0a69133737bfcf0c35b6f02efbca28dd1910e846
mihret-emeru <mercyemeru@gmail.com>
2026-07-31 20:24:26 +0300

    Add formatLeadSource function for lead sources

--- src/utils/LeadFormat.js
+++ src/utils/LeadFormat.js
@@ -1,1 +1,13 @@
+export function formatLeadSource(source) {
+  const sources = {
+    facebook_ad: "Facebook Ad",
+    client_registration: "Client Registration",
+    phone_call: "Phone Call",
+    office_visit: "Office Visit",
+    referral: "Referral",
+    website: "Website",
+  };
+
+  return sources[source] || source;
+}
 
