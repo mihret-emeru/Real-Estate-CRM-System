95edce724c6f5c20080877337a1db98aace07173
Mercy <mercyemeru@gmail.com>
2026-08-10 20:12:56 +0300

    Add chartColors object with color definitions

--- src/utils/chartColors.js
+++ src/utils/chartColors.js
@@ -1,1 +1,15 @@
+export const chartColors = {
+  leads: "#45B5C4",
+  sales: "#D9ABFF",
+  revenue: "#F9D423",
+  payments: "#3F8F8A",
+  properties: "#FB6B41",
+
+  success: "#228B22",
+  warning: "#E0A23B",
+  danger: "#FE4365",
+  neutral: "#7D8793",
+
+  palette: ["#45B5C4", "#D9ABFF", "#F9D423", "#3F8F8A", "#FB6B41", "#FE4365"],
+};
 
