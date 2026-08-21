f4944b59da22d4ca561051c3ccb6a6261792faea
Mercy <mercyemeru@gmail.com>
2026-08-21 23:32:14 +0300

    Add ClientLayout component using DashboardLayout

--- src/app/client/layout.js
+++ src/app/client/layout.js
@@ -1,1 +1,6 @@
+import DashboardLayout from "@/components/dashboard/DashboardLayout";
+
+export default function ClientLayout({ children }) {
+  return <DashboardLayout>{children}</DashboardLayout>;
+}
 
