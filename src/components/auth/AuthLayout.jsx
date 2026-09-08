f5845e13cea8513dd1e322c86e6dc078dcadcc15
Mercy <mercyemeru@gmail.com>
2026-09-08 23:46:35 +0300

    Refactor SVG path for cleaner syntax

--- src/components/auth/AuthLayout.jsx
+++ src/components/auth/AuthLayout.jsx
@@ -7,17 +7,7 @@ export default function AuthLayout({ leftContent, rightContent }) {
         viewBox="0 0 180 1000"
         preserveAspectRatio="none"
       >
-        <path
-          d="
-      M180 0
-      C60 30, 0 160, 40 350
-      C75 480, 75 520, 40 650
-      C0 840, 60 970, 180 1000
-      L180 1000
-      L180 0
-      Z
-    "
-        />
+        <path d="M180 0 C60 30, 0 160, 40 350 C75 480, 75 520, 40 650 C0 840, 60 970, 180 1000 L180 1000 L180 0 Z" />
       </svg>
 
       <div className="auth-right">{rightContent}</div>
